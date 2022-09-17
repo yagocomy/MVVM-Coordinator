c4c5afeda9689deb08870812b45bb77bd408e1c5
yagocomy <yagodemourasilva@gmail.com>
2022-09-17 14:44:15 -0300

    implementacao de arquitetura

--- MVVM+Coordinator/Screen A/ViewModel/ScreenAViewModel.swift
+++ MVVM+Coordinator/Screen A/ViewModel/ScreenAViewModel.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+class ScreenAViewModel {
+    
+    init(){
+        
+    }
+}
--- MVVM+Coordinator/Screen B/ViewModel/ScreenBViewModel.swift
+++ MVVM+Coordinator/Screen B/ViewModel/ScreenBViewModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+class ScreenBViewModel {
+    
+    init(){
+    }
+}
