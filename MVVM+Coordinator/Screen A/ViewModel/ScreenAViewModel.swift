83fc203cbfea5aaa6925476a04afe9c8c5477428
yagocomy <yagodemourasilva@gmail.com>
2022-09-17 14:40:52 -0300

    inicio a implementacao de arquitetura MVVM

---
+++ MVVM+Coordinator/Screen A/ViewModel/ScreenAViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ScreenAViewModel.swift
+//  MVVM+Coordinator
+//
+//  Created by Yago De Moura Silva on 17/09/22.
+//
+
+import Foundation
---
+++ MVVM+Coordinator/Screen C/ViewModel/ViewMOdel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewMOdel.swift
+//  MVVM+Coordinator
+//
+//  Created by Yago De Moura Silva on 17/09/22.
+//
+
+import Foundation
